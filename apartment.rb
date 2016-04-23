fe61c5ce7765777566875361014a97a77c6d8af3
hannataylor <hanna.taylor91@gmail.com>
2016-04-23 18:51:44 -0400

    Complete Challenge

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,34 @@
-
+require 'pry'
 class Apartment
+  attr_accessor :monthly_rent, :number, :rooms
+
+  def initialize(args={})
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+    @number = args.fetch(:number,'')
+    @rooms = args.fetch(:rooms,'')
+  end
+
+  def total_sqft
+    # binding.pry
+    sq_footage = rooms.map do |room|
+      room.sqft
+    end
+    sq_footage.reduce(:+)
+  end
+
+  def price_per_sqft
+    monthly_rent/total_sqft.to_f
+  end
+
+  def room_count
+    rooms.length
+  end
+
+  def bedroom_count
+    bedrooms = rooms.select {|room| room.name == "bedroom"}
+    bedrooms.length
+  end
+
+
 
 end
--- room.rb
+++ room.rb
@@ -1,3 +1,9 @@
 class Room
+  attr_accessor :name, :sqft, :monthly_rent
 
+  def initialize(args={})
+    @name = args.fetch(:name, "bedroom")
+    @sqft = args.fetch(:sqft, 120)
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
 end
