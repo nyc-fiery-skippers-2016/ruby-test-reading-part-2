4c7314ef07046dac67759306eb83f358d917a3dc
Christyn Budzyna <cbudzyna@gmail.com>
2016-04-24 13:28:17 -0400

    Finish challenge except for sqft method

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,30 @@
+require_relative 'room'
 
 class Apartment
+attr_reader :rooms, :monthly_rent
+
+  def initialize(args = {})
+    @rooms = args[:rooms]
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    @rooms.reduce(0) do |sum, room|
+      sum += room.sqft
+    end
+  end
+
+  def price_per_sqft
+    self.monthly_rent / total_sqft.to_f
+  end
+
+  def room_count
+    self.rooms.length
+  end
+
+  def bedroom_count
+    bedroom_array = @rooms.select {|room| room.name == "bedroom"}
+    bedroom_array.length
+  end
 
 end
--- building.rb
+++ building.rb
@@ -1,4 +1,69 @@
+require_relative 'apartment'
+require_relative 'room'
 
 class Building
 
+attr_reader :apartments
+
+  def initialize(args = {})
+    @apartments = args[:apartments]
+  end
+
+  def total_room_count
+    @apartments.reduce(0) {|sum, apartment|
+      sum += apartment.rooms.length}
+  end
+
+  def total_monthly_revenue
+    @apartments.reduce(0) {|sum, apartment|
+      sum += apartment.monthly_rent }
+  end
+
+  def apartments_by_rent
+    self.apartments.sort_by{|apartment| -apartment.monthly_rent}
+  end
+
+  def find_apartments_by_bedroom_count(num)
+    apt_array = self.apartments.select {|apt|
+      apt.bedroom_count == num}
+    apt_array[0] #This is here because the rspec was expecting it not to be in an array
+  end
+
+  def total_sqft
+  @apartments.each do |apt|
+    apt.rooms.reduce do |sum, room|
+      sum += room.sqft
+      #This method is not working - the rspec is saying that "+" is an undefined method for the room object.  I thought that I was selecting the square feet of the room object.  This works in my tests below but not in the rspec.  Can you advise?
+    end
+  end
 end
+
+end
+
+# room1 = Room.new(name: "bathroom", sqft: 120)
+# room2 = Room.new(name: "bedroom", sqft: 140)
+# room3 = Room.new(name: "living room", sqft: 120)
+# room4 = Room.new(name: "dining room", sqft: 140)
+
+# apartment1 = Apartment.new(rooms: [room1,room2], monthly_rent: 1000)
+# apartment2 = Apartment.new(rooms: [room3,room4], monthly_rent: 1000)
+
+# building1 = Building.new(apartments: [apartment1,apartment2])
+
+
+# # building1.apartments - array of apartment objects
+
+# # building1.apartments.each do |apt|
+# #   p apt.rooms # prints 2 arrays of 2 rooms
+# # end
+
+#     building1.apartments.each do |apt|
+#     apt.rooms.reduce(0) do |sum,room|
+#     sum += room.sqft
+#   end
+# end
+
+
+# # building1.apartments.each do |apt|
+# #   p apt.bedroom_count
+# # # end
