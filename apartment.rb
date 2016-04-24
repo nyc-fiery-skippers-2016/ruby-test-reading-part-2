079e23834d770b8a99bc7cfbd90efedc66b9e718
NYCSwan <megan.swanby@gmail.com>
2016-04-24 13:06:48 -0400

    Add all working methods, bug in find_apartments_by_bedroom

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,28 @@
+require_relative "room"
+require "pry"
 
 class Apartment
+	attr_reader :rooms, :monthly_rent
+
+	def initialize(args={})
+		@rooms = args[:rooms]
+		@monthly_rent = args[:monthly_rent] || 1000
+	end
+
+	def total_sqft
+		rooms.reduce(0) {|sum, room| sum + room.sqft } 
+	end
+
+	def price_per_sqft
+		monthly_rent.to_f / total_sqft
+	end
+
+	def room_count
+		rooms.length
+	end
+
+	def bedroom_count
+		rooms.count { |name| name == "bedroom" }
+	end
 
 end
--- building.rb
+++ building.rb
@@ -1,4 +1,32 @@
-
+require_relative "apartment"
 class Building
+	attr_reader :apartments
 
+	def initialize(args={})
+		@apartments = args[:apartments] 
+
+	end
+
+	def total_room_count
+		apartments.reduce(0) {|sum, apartment| sum + apartment.rooms.length }
+	end
+
+	def total_monthly_revenue
+		apartments.reduce(0) { |sum, apartment| sum + apartment.monthly_rent }
+	end
+
+	def apartments_by_rent
+		apartments.sort {|a,b| b.monthly_rent <=> a.monthly_rent}
+	end
+
+	def find_apartments_by_bedroom_count(num)
+		apartments.select do |apartment| 
+	require 'pry'; binding.pry
+			apartment.bedroom_count == num 
+		end
+	end
+
+	def total_sqft
+		apartments.reduce(0) {|sum, apartment| sum + apartment.total_sqft}
+	end
 end
