2ec469936b2514264b8d17f1566b79298a5851a6
sergener <sibel.ergener@gmail.com>
2016-04-24 17:02:24 -0400

    Add all features to Room and Apartment class to pass tests

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,28 @@
+require_relative 'room'
 
 class Apartment
+attr_reader :number, :rooms, :monthly_rent
+
+	def initialize(args={})
+		@number = args[:number]
+		@rooms = args[:rooms]
+		@monthly_rent = args[:monthly_rent] || 1000
+	end
+
+	def total_sqft #this is an array of rooms
+		ttl_sqft = @rooms.reduce(0) {|ttl_sqft, room| ttl_sqft += room.sqft}
+	end
+
+	def price_per_sqft
+		@monthly_rent.fdiv(total_sqft)
+	end
+
+	def room_count
+		@rooms.length
+	end
+
+	def bedroom_count
+		@rooms.count {|room| room.name == "bedroom"}
+	end
 
 end
--- room.rb
+++ room.rb
@@ -1,3 +1,11 @@
 class Room
+attr_reader :sqft, :window_count
+attr_accessor :name
+
+def initialize(args={})
+	@name = args[:name] || "bedroom"
+	@sqft = args[:sqft] || 120
+	@window_count = args[:window_count]
+end
 
 end
