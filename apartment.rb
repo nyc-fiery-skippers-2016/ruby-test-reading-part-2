5993f77e2e61c778b381cc96e95782b407a6aa48
Esther Leytush <eleytush@gmail.com>
2016-04-24 21:55:14 -0400

    add initial solution

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,26 @@
 
 class Apartment
+  attr_reader :number, :rooms, :monthly_rent
 
+  def initialize(args={})
+    @number = args.fetch(:number) { "no number" }
+    @rooms = args.fetch(:rooms) { 2 }
+    @monthly_rent = args.fetch(:monthly_rent) { 1000 }
+  end
+
+  def total_sqft
+    rooms.map{|room| room.sqft}.inject(:+)
+  end
+
+  def price_per_sqft
+    (monthly_rent.to_f / total_sqft.to_f).round(1)
+  end
+
+  def room_count
+    rooms.length
+  end
+
+  def bedroom_count
+    rooms.select{|room| room.name == 'bedroom'}.length
+  end
 end
--- building.rb
+++ building.rb
@@ -1,4 +1,29 @@
 
 class Building
+  attr_reader :address, :apartments
 
+  def initialize(args={})
+    @address = args.fetch(:address)
+    @apartments = args.fetch(:apartments)
+  end
+
+  def total_room_count
+    apartments.map{|apt| apt.rooms.length}.inject(:+)
+  end
+
+  def total_monthly_revenue
+    apartments.map{|apt| apt.monthly_rent}.inject(:+)
+  end
+
+  def apartments_by_rent
+    apartments.sort_by{|apt| apt.monthly_rent}.reverse
+  end
+
+  def find_apartments_by_bedroom_count(num)
+    apartments.find{|apt| apt.bedroom_count == num}
+  end
+
+  def total_sqft
+    apartments.map{|apt| apt.total_sqft}.inject(:+)
+  end
 end
