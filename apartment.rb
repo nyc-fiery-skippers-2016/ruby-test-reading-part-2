d430f1eb8b558dfbdec6aa0325cf835e4b23178b
Samantha Holmes <email": null>
2016-04-25 16:18:44 -0400

    Finish ruby test reading challenge

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,31 @@
 class Apartment
+
+  attr_accessor :number, :monthly_rent, :rooms
+
+  def initialize(args={})
+    @number = args[:number]
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+    @rooms = args[:rooms]
+  end
+
+  def total_sqft
+    sqft = []
+    @rooms.select {|room| sqft << room.sqft}
+    return sqft.reduce(:+)
+  end
+
+  def price_per_sqft
+    @monthly_rent.fdiv(total_sqft)
+  end
+
+  def room_count
+    @rooms.length
+  end
+
+  def bedroom_count
+    name = []
+    @rooms.select {|room| name << room.name if room.name == 'bedroom'}
+    name.length
+  end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,37 @@
+require_relative 'apartment'
+
 class Building
+
+  attr_accessor :apartments
+
+  def initialize(args={})
+    @apartments = args[:apartments]
+  end
+
+  def total_room_count
+    all_rooms = []
+    @apartments.select {|apartment| all_rooms << apartment.rooms}
+    all_rooms.flatten.length
+  end
+
+  def total_monthly_revenue
+    all_rents = []
+    @apartments.select {|apartment| all_rents << apartment.monthly_rent}
+    all_rents.reduce(:+)
+  end
+
+  def apartments_by_rent
+    apartments_by_rent = @apartments.sort {|apartment1, apartment2| apartment2.monthly_rent <=> apartment1.monthly_rent}
+    apartments_by_rent
+  end
+
+  def find_apartments_by_bedroom_count(num_of_bedrooms)
+    @apartments.find {|apartment| apartment.bedroom_count == num_of_bedrooms}
+  end
+
+  def total_sqft
+    building_sqft = []
+    @apartments.select {|apartment| building_sqft <<  apartment.total_sqft}
+    building_sqft.reduce(:+)
+  end
 end
