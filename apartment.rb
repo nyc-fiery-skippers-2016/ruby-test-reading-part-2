229b47ba1fa3fe4be1c25e05a6c393d9814f1a7d
Diana Ozemebhoya Eromosele <email": null>
2016-04-24 23:41:54 -0400

    Update to Apartment-Room Challenge

--- apartment.rb
+++ apartment.rb
@@ -9,7 +9,7 @@ class Apartment
   def initialize(args={})
     @number = args[:number]
     @monthly_rent = args[:monthly_rent] || 1000
-    @rooms = []
+    @rooms = [Room.new(name: 'kitchen', sqft: 120, window_count: 3), Room.new(name: 'lounge', sqft: 380, window_count: 2)]
   end
 
   def total_sqft
@@ -19,8 +19,9 @@ class Apartment
   end
 
   def price_per_sqft
-    price = self.monthly_rent / total_sqft
-    price.to_f.round(4) # this still won't show me the complete answer up to decimal points. It shoudl return 2.9, not 2.
+    @monthly_rent / total_sqft
+    # price
+    # price.to_f # this still won't show me the complete answer up to decimal points. It shoudl return 2.9, not 2.
     # sprintf('%.2f', price.to_d)
   end
 
@@ -41,10 +42,12 @@ class Apartment
 end
 
 
+new_apartment = Apartment.new(number: '4G', rooms: [Room.new(name: 'kitchen', sqft: 120, window_count: 3), Room.new(name: 'lounge', sqft: 380, window_count: 2)], monthly_rent: 1450)
+
 # p new_apartment.rooms
 # p new_apartment.monthly_rent
 # p new_apartment.total_sqft
-# p new_apartment.price_per_sqft ## why won't this print 2.9? i've used to_f,
+p new_apartment.price_per_sqft ## why won't this print 2.9? i've used to_f,
 # p new_apartment.room_count
 # p new_apartment.bedroom_count
 
