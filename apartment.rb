f115b10597e318d8532af86b329afe583ced3c32
AlanaFarkas <email": null>
2016-04-25 18:12:49 -0400

    break

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,31 @@
+require 'pry'
 class Apartment
+  attr_reader :rooms, :monthly_rent
+
+  def initialize(args={})
+    @rooms = args.fetch(:rooms, "none given")
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    array = []
+    rooms.each do |room_in_apt|
+      array << room_in_apt.sqft
+    end
+    array.reduce(:+)
+  end
+
+  def price_per_sqft
+    monthly_rent/total_sqft.to_f
+  end
+
+  def room_count
+    rooms.count
+  end
+
+  def bedroom_count
+    rooms.select { |room_in_apt| room_in_apt.name == 'bedroom' }.count
+  end
+
 end
+
--- building.rb
+++ building.rb
@@ -1,2 +1,13 @@
 class Building
+attr_reader :address, :apartments
+
+  def initialize(args={})
+    @address = args.fetch(:address, "none given")
+    @apartments = args.fetch(:apartments, "none given")
+  end
+
+  def total_room_count
+
+  end
+
 end
