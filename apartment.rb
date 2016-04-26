2c0839d525ba4d3db1888c01ed0ea357997c6a7e
Katherine Broner <katherine.broner@gmail.com>
2016-04-25 20:11:53 -0400

    Complete reading tests 2 challenge

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,27 @@
 class Apartment
+
+  attr_reader :monthly_rent, :rooms
+
+  def initialize(args={})
+    @monthly_rent = args[:monthly_rent] || 1000
+    @rooms = args[:rooms]
+  end
+
+  def total_sqft
+    @rooms.reduce(0) { |sum, room| sum + room.sqft}
+  end
+
+  def price_per_sqft
+    @monthly_rent / total_sqft.to_f
+  end
+
+  def room_count
+    @rooms.count
+  end
+
+  def bedroom_count
+    bedrooms = @rooms.select { |room| room.name == 'bedroom'}
+    bedrooms.count
+  end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,30 @@
 class Building
+
+  attr_reader :apartments
+
+  def initialize(args={})
+    @apartments = args[:apartments]
+  end
+
+  def total_room_count
+    @apartments.reduce(0) { |sum, apartment| sum + apartment.rooms.count }
+  end
+
+  def total_monthly_revenue
+    @apartments.reduce(0) { |sum, apartment| sum + apartment.monthly_rent}
+  end
+
+  def apartments_by_rent
+    @apartments.sort { |low, high| high.monthly_rent <=> low.monthly_rent }
+  end
+
+  def find_apartments_by_bedroom_count(count)
+    @apartments.find do |apartment|
+      apartment.bedroom_count == count
+    end
+  end
+
+  def total_sqft
+    @apartments.reduce(0) { |sum, apartment| sum + apartment.total_sqft }
+  end
 end
