fa9e878bf1c90446c047d393db50b5923c919af9
notjoeyoo <joseph_yoo@brown.edu>
2016-04-26 10:28:03 -0400

    finish

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,27 @@
 class Apartment
+  attr_reader :number,:rooms,:monthly_rent
+  def initialize(args={})
+    @number = args[:number]
+    @rooms = args[:rooms] || []
+    @monthly_rent = args[:monthly_rent]||1000
+  end
+
+  def total_sqft
+    sum = 0
+    rooms.each{|room|sum+=room.sqft}
+    sum
+  end
+
+  def price_per_sqft
+    monthly_rent.to_f/total_sqft.to_f
+  end
+
+  def room_count
+    rooms.count
+  end
+
+  def bedroom_count
+    rooms.select{|room|room.name=='bedroom'}.count
+  end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,33 @@
 class Building
+  attr_reader :apartments, :address
+  def initialize(args={})
+    @address = args[:address]
+    @apartments = args[:apartments]
+  end
+
+  def total_room_count
+    sum = 0
+    apartments.each{|apt|sum += apt.room_count}
+    sum
+  end
+
+  def total_monthly_revenue
+    sum = 0
+    apartments.each{|apt|sum += apt.monthly_rent}
+    sum
+  end
+
+  def apartments_by_rent
+    apartments.sort_by{|apt|apt.monthly_rent}.reverse
+  end
+
+  def find_apartments_by_bedroom_count(count)
+    apartments.find{|apt|apt.bedroom_count==count} #assuming not to return more than one apt because an array is not expected
+  end
+
+  def total_sqft
+    sum = 0
+    apartments.each{|apt|sum += apt.total_sqft}
+    sum
+  end
 end
