9af5b38f7dedcfa611542003f018551aa9f8b00b
Caitlin Hoffman <choff999@gmail.com>
2016-04-26 17:30:38 -0400

    Added solution to challenge

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,29 @@
+require_relative 'room'
 
 class Apartment
+	attr_accessor :number, :rooms, :monthly_rent
+
+	def initialize(args={})
+		@number = args.fetch(:number,'10#7')
+		@rooms = args[:rooms]
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+	end
+
+	def total_sqft
+		self.rooms.map {|room| room.sqft}.reduce{|sum, num| sum += num}
+	end
+
+	def price_per_sqft
+		self.monthly_rent.to_f/total_sqft.to_f
+	end
+
+	def room_count
+		self.rooms.count
+	end
+
+	def bedroom_count
+		self.rooms.select {|room| room.name == "bedroom".downcase}.count
+		#or can do .length
+	end
 
 end
--- building.rb
+++ building.rb
@@ -1,4 +1,36 @@
+require_relative 'apartment'
 
 class Building
+	attr_accessor :apartments
+	def initialize(args={})
+		@address = args[:address]
+		@apartments = args[:apartments]
+	end
+	#fetch gives you an auto answer
+
+	def total_room_count
+		self.apartments.map {|apartment| apartment.room_count}.reduce{|sum, num| sum+=num}
+	#so you want to return an array of apartments where it counts each room for each apartment, and then adds all the rooms together with reduce for building
+	end
+	
+	def total_monthly_revenue
+		self.apartments.map {|apartment| apartment.monthly_rent}.reduce{|sum, num| sum+=num}
+	#goes through creates array with apartments and finds their rent, then adds all rents together to give total rent for building
+	end
+
+	def apartments_by_rent
+		self.apartments.sort_by {|apartment| apartment.monthly_rent}.reverse
+	#you want to apartments to be sorted by monthly_rent so iterate through with sort-by and then you need reverse because the rspec says from largest to smallest
+	end
+
+	def	find_apartments_by_bedroom_count(input)
+		self.apartments.find {|apartment| apartment.bedroom_count == input}
+	#if you input a number it will find you an apartment that matches that bedroom count
+	end
+
+	def total_sqft
+		self.apartments.map {|apartment| apartment.total_sqft}.reduce{|sum, num| sum +=num}
+	#map iterates through the apartments and finds total sqft for each apartment which is the total sqft method for rooms in apartment class and then adds them up via reduce to give building sqft
+	end
 
 end
