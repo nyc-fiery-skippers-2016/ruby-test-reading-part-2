134fb0c7b0f195b0d698f5be0781a97f4af7ee0e
d-walden <david.ny.walden@gmail.com>
2016-04-25 22:40:54 -0400

    Complete ruby-test-reading-part-2 by David Walden

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,26 @@
 class Apartment
+  attr_reader :number, :rooms, :monthly_rent
+
+  def initialize(args = {})
+    @number = args.fetch(:number, '#1')
+    @rooms = args.fetch(:rooms, 1)
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    @rooms.inject(0) {|total, room| total + room.sqft}
+  end
+
+  def price_per_sqft
+    @monthly_rent/total_sqft.to_f
+  end
+
+  def room_count
+    @rooms.length
+  end
+
+  def bedroom_count
+    @rooms.count {|room| room.name == 'bedroom'}
+  end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,30 @@
 class Building
+
+  attr_reader :address, :apartments
+
+  def initialize(args = {})
+    @address = args.fetch(:address)
+    @apartments = args.fetch(:apartments)
+  end
+
+  def total_room_count
+    @apartments.inject(0) {|total, apartment| total + apartment.room_count}
+  end
+
+  def total_monthly_revenue
+    @apartments.inject(0) {|total, apartment| total + apartment.monthly_rent}
+  end
+
+  def apartments_by_rent
+    @apartments.sort_by {|apartment| apartment.monthly_rent}.reverse
+  end
+
+  def find_apartments_by_bedroom_count(bedrooms)
+    @apartments.find { |apartment| apartment.bedroom_count == bedrooms}
+  end
+
+  def total_sqft
+    @apartments.inject(0) {|total, apartment| total + apartment.total_sqft}
+  end
+
 end
