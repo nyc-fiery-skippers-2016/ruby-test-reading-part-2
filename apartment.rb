869c595ae682e9f6933b5671419573ef14baeaa4
Samantha Holmes <samanthavholmes@gmail.com>
2016-04-26 16:51:24 -0400

    Refactor code

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,27 @@
+
 class Apartment
+
+  attr_accessor :number, :monthly_rent, :rooms
+
+  def initialize(args={})
+    @number = args[:number]
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+    @rooms = args[:rooms]
+  end
+
+  def total_sqft
+    @rooms.reduce(0) {|sum, room| sum += room.sqft}
+  end
+
+  def price_per_sqft
+    @monthly_rent.fdiv(total_sqft)
+  end
+
+  def room_count
+    @rooms.length
+  end
+
+  def bedroom_count
+    @rooms.select {|room| room.name == 'bedroom'}.length
+  end
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,30 @@
+require_relative 'apartment'
+
 class Building
+
+  attr_accessor :apartments
+
+  def initialize(args={})
+    @apartments = args[:apartments]
+  end
+
+  def total_room_count
+    @apartments.map {|apartment| apartment.rooms}.flatten.length
+  end
+
+  def total_monthly_revenue
+    @apartments.reduce(0) {|sum, apartment|sum += apartment.monthly_rent}
+  end
+
+  def apartments_by_rent
+   @apartments.sort {|apartment1, apartment2| apartment2.monthly_rent <=> apartment1.monthly_rent}
+  end
+
+  def find_apartments_by_bedroom_count(num_of_bedrooms)
+    @apartments.find {|apartment| apartment.bedroom_count == num_of_bedrooms}
+  end
+
+  def total_sqft
+    @apartments.reduce(0) {|sum, apartment| sum += apartment.total_sqft}
+  end
 end
