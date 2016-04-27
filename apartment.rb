ca24d05dd9316eb89b712f727bd85e8900e09b06
Samantha Holmes <samanthavholmes@gmail.com>
2016-04-27 12:04:33 -0400

    Use attr_readers instead of instance variables in methods

--- apartment.rb
+++ apartment.rb
@@ -9,18 +9,18 @@ class Apartment
   end
 
   def total_sqft
-    @rooms.reduce(0) {|sum, room| sum += room.sqft}
+    rooms.reduce(0) {|sum, room| sum += room.sqft}
   end
 
   def price_per_sqft
-    @monthly_rent.fdiv(total_sqft)
+    monthly_rent.fdiv(total_sqft)
   end
 
   def room_count
-    @rooms.length
+    rooms.length
   end
 
   def bedroom_count
-    @rooms.select {|room| room.name == 'bedroom'}.length
+    rooms.select {|room| room.name == 'bedroom'}.length
   end
 end
--- building.rb
+++ building.rb
@@ -9,33 +9,31 @@ class Building
   end
 
   def total_room_count
-    @apartments.map {|apartment| apartment.rooms}.flatten.length
+    apartments.map {|apartment| apartment.rooms}.flatten.length
   end
 
   def total_monthly_revenue
-    @apartments.reduce(0) {|sum, apartment|sum += apartment.monthly_rent}
+    apartments.reduce(0) {|sum, apartment|sum += apartment.monthly_rent}
   end
 
   def apartments_by_rent
-   @apartments.sort {|apartment1, apartment2| apartment2.monthly_rent <=> apartment1.monthly_rent}
+   apartments.sort {|apartment1, apartment2| apartment2.monthly_rent <=> apartment1.monthly_rent}
   end
 
   def total_monthly_revenue
-    all_rents = []
-    @apartments.select {|apartment| all_rents << apartment.monthly_rent}
-    all_rents.reduce(:+)
+    apartments.reduce(0) {|sum, apartment| sum += apartment.monthly_rent}
   end
 
   def apartments_by_rent
-    @apartments.sort {|apartment1, apartment2| apartment2.monthly_rent <=> apartment1.monthly_rent}
+    apartments.sort {|apartment1, apartment2| apartment2.monthly_rent <=> apartment1.monthly_rent}
   end
 
   def find_apartments_by_bedroom_count(num_of_bedrooms)
-    @apartments.find {|apartment| apartment.bedroom_count == num_of_bedrooms}
+    apartments.find {|apartment| apartment.bedroom_count == num_of_bedrooms}
   end
 
   def total_sqft
-    @apartments.reduce(0) {|sum, apartment| sum += apartment.total_sqft}
+    apartments.reduce(0) {|sum, apartment| sum += apartment.total_sqft}
   end
 end
 
